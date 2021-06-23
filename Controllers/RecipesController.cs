5e1eb13aa6f0ec845a590040ef0b29e68783ccb8
Artem <a.khimin@yandex.ru>
2021-06-23 23:31:12 +0300

    Добавил метод DeleteRecipes

---
+++ Controllers/RecipesController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        [HttpGet]
+        public string[] GetDishes()
+        {
+            string[] dishes = { "Oxtail", "Curry Chicken", "Dumplings" };
+            return dishes;
+        }
+
+        [HttpGet]
+        public ActionResult GetRecipes()
+        {
+            string[] recipes = { "Oxtail", "Curry Chicken", "Dumplings" };
+
+            if (recipes.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(recipes);
+        }
+
+
+        [HttpDelete]
+        public ActionResult DeleteRecipes()
+        {
+            bool badThingsHappened = false;
+
+            if (badThingsHappened)
+                return BadRequest();
+            return NoContent();
+
+        }
+
+    }
+}
